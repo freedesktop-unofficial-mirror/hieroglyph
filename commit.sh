9f0914d35f1d2320b632a6652740391672346138
Akira TAGOH <akira@tagoh.org>
2006-05-05 16:37:24 +0000

    "	* commit.sh: implemented a testrun mode and -m option to give the own 	commit message."

--- commit.sh
+++ commit.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+DO=""
 TOPDIR=`dirname $0`
 VERSIONIN=$TOPDIR/hieroglyph/version.h.in.in
 VERSION_H=${VERSIONIN//.in.in}.in
@@ -7,14 +8,25 @@ if [ "x`svn stat`" = "x" ]; then
     echo "No changes has been made. any changes has never committed."
     exit
 fi
-$TOPDIR/update-version
+if [ "x$DO" = "x" ]; then
+    $TOPDIR/update-version
+fi
+LOG=''
+if test "$1" = "--message" || test "$1" = "-m"; then
+    shift
+    LOG="$1"
+    shift
+fi
 FILES=''
 while [ $# -ne 0 ]; do
-	FILES="$FILES $1"
-	shift
+    FILES="$FILES $1"
+    shift
 done
 if [ "x$FILES" != "x" ]; then
-	FILES="$FILES ChangeLog $VERSION_H"
+    FILES="$FILES ChangeLog $VERSION_H"
+fi
+if [ "x$LOG" = "x" ]; then
+    LOG="`svn diff | awk 'BEGIN{P=0;F=0}$0 ~ /^+++ ChangeLog/{F=1}F == 1 && $0 ~ /^@@/{P=1}P == 1 && $0 ~ /^+/{print $0}P == 1 && /^Index/{P=0;F=0}' | sed -e 's/^+//'`"
 fi
 
-LANG=C svn ci -m "`svn diff | awk 'BEGIN{P=0;F=0}$0 ~ /^+++ ChangeLog/{F=1}F == 1 && $0 ~ /^@@/{P=1}P == 1 && $0 ~ /^+/{print $0}P == 1 && /^Index/{P=0;F=0}' | sed -e 's/^+//'`" $FILES
+LANG=C $DO svn ci -m "\"$LOG\"" $FILES
