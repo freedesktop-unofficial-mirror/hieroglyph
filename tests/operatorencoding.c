1a782d8030433373f4851a79202e20699bdebe84
Akira TAGOH <akira@tagoh.org>
2006-07-16 06:46:14 +0000

    * src/Makefile.am: removed the libretto.la dependency.

--- tests/operatorencoding.c
+++ tests/operatorencoding.c
@@ -1,18 +1,18 @@
-#include <libretto/lbtypes.h>
+#include <hieroglyph/operator.h>
 
 int
 main(void)
 {
-	if (LB_op_setpattern != 225) {
-		g_print("actual result: setpattern = %d\n", LB_op_store);
+	if (HG_op_setpattern != 225) {
+		g_print("actual result: setpattern = %d\n", HG_op_store);
 		return 1;
 	}
-	if (LB_op_sym_eq != 256) {
-		g_print("actual result: /= = %d\n", LB_op_sym_eq);
+	if (HG_op_sym_eq != 256) {
+		g_print("actual result: /= = %d\n", HG_op_sym_eq);
 		return 1;
 	}
-	if (LB_op_POSTSCRIPT_RESERVED_END != 481) {
-		g_print("actual result: %d\n", LB_op_POSTSCRIPT_RESERVED_END);
+	if (HG_op_POSTSCRIPT_RESERVED_END != 481) {
+		g_print("actual result: %d\n", HG_op_POSTSCRIPT_RESERVED_END);
 		return 1;
 	}
 
