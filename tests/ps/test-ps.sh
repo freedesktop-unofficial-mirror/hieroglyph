eb3c03df98615de3d3dd9be4a4ee613ae6a3863a
Akira TAGOH <akira@tagoh.org>
2006-09-19 19:21:26 +0000

    * tests/ps/test-ps.sh: depends on hgs instead of pse.
    	* tests/ps/Makefile.am (check): run test-ps.sh.

--- tests/ps/test-ps.sh
+++ tests/ps/test-ps.sh
@@ -3,5 +3,5 @@
 rootdir=`dirname $0`/../..
 export HIEROGLYPH_LIB_PATH=$rootdir/plugins/test
 for i in `dirname $0`/test-*.ps; do
-    $rootdir/tests/run.sh $rootdir/examples/pse -l test $i
+    $rootdir/tests/run.sh $rootdir/src/hgs -l test $i
 done
