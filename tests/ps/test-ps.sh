19fa80cca9762cffb6daf8b4285a3c9a99076488
Akira TAGOH <akira@tagoh.org>
2006-09-23 16:50:32 +0000

    2006-09-24  Akira TAGOH  <at@gclab.org>
    	* plugins/test/hg_unittest.ps (typecheck): new procedure to test
    	if /typecheck happens.
    	(.dotypecheck): new.

--- tests/ps/test-ps.sh
+++ tests/ps/test-ps.sh
@@ -5,5 +5,6 @@ set -e
 rootdir=`dirname $0`/../..
 export HIEROGLYPH_LIB_PATH=$rootdir/plugins/test
 for i in `dirname $0`/test-*.ps; do
+    echo $i
     $rootdir/tests/run.sh $rootdir/src/hgs -l test $i
 done
