40ffbddeb1195ee72aa52c25ce768ad0b6c8ff96
Akira TAGOH <akira@tagoh.org>
2010-07-01 22:39:05 +0900

    trivial fix

--- hieroglyph/hgobject.h
+++ hieroglyph/hgobject.h
@@ -4,7 +4,7 @@
  * Copyright (C) 2005-2010 Akira TAGOH
  * 
  * Authors:
- *   Akira TAGOH  <tagoh@redhat.com>
+ *   Akira TAGOH  <akira@tagoh.org>
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
