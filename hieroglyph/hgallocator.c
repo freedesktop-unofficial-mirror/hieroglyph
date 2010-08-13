7725c5609f6c235b76f47429e1d7f72880a5ec4b
Akira TAGOH <akira@tagoh.org>
2010-08-13 18:05:05 +0900

    trivial update for debugging

--- hieroglyph/hgallocator.c
+++ hieroglyph/hgallocator.c
@@ -25,6 +25,7 @@
 #include "config.h"
 #endif
 
+#include <stdlib.h>
 #include <string.h>
 #include "hgerror.h"
 #include "hgquark.h"
@@ -755,6 +756,7 @@ _hg_allocator_gc_finish(hg_allocator_data_t *data,
 
 		used_size = data->used_size - priv->slave.used_size;
 
+#define HG_GC_DEBUG
 #if defined (HG_DEBUG) && defined (HG_GC_DEBUG)
 		g_print("GC: marking the locked objects\n");
 #endif
@@ -776,6 +778,9 @@ _hg_allocator_gc_finish(hg_allocator_data_t *data,
 					if (block->lock_count > 0) {
 						g_warning("[BUG] non-Unlocked index 0x%lx [size: %ld, count: %d]\n",
 							  i + 1, block->size, block->lock_count);
+#if defined (HG_DEBUG) && defined (HG_GC_DEBUG)
+						abort();
+#endif
 						was_error = !_hg_allocator_gc_mark(data, i + 1, NULL);
 						if (was_error)
 							break;
@@ -789,9 +794,10 @@ _hg_allocator_gc_finish(hg_allocator_data_t *data,
 
 #if defined (HG_DEBUG) && defined (HG_GC_DEBUG)
 	if (!was_error)
-		g_print("GC: %ld -> %ld (%ld bytes freed)\n",
+		g_print("GC: %ld -> %ld (%ld bytes freed) / %ld\n",
 			data->used_size, priv->slave.used_size,
-			data->used_size - priv->slave.used_size);
+			data->used_size - priv->slave.used_size,
+			data->total_size);
 	else
 		g_print("GC: failed.\n");
 #endif
